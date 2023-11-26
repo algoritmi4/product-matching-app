79166819126e7a567037d4fa7377a56c804d0b39
kirill-k88 <kirill-k88@ya.ru>
2023-11-26 16:53:54 +0300

    decorate Msrking Page

--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import Select, { components } from 'react-select';
 import './Selector.css';
 
 export function Selector({
@@ -10,18 +8,16 @@ export function Selector({
   setMatchCount: any;
 }) {
   const handleChange = (e: any) => {
-    console.log(e.target.value);
-
     setMatchCount(Number(e.target.value));
   };
 
   return (
     <select className="select" defaultValue={matchCount} onChange={handleChange}>
-      <option value="2">Количество 2</option>
-      <option value="3">Количество 3</option>
-      <option value="5">Количество 5</option>
-      <option value="10">Количество 10</option>
-      <option value="20">Количество 20</option>
+      <option value="2">Вариантов: 2</option>
+      <option value="3">Вариантов: 3</option>
+      <option value="5">Вариантов: 5</option>
+      <option value="10">Вариантов: 10</option>
+      <option value="20">Вариантов: 20</option>
       <option value="999">Выбрать вручную</option>
     </select>
   );
